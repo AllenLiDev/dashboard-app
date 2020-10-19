97b2f23498f1857f93d844fa5ac70dfee1f9b399
Allen <headhuntar@gmail.com>
2020-10-19 14:44:15 -0700

    add code to get equipped runes id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import testData from './headhuntar_swarfarm_win10_optimizer_export.json';
 
+// get all unequiped runes
 const RuneList = (props) => {
   const runes = testData.runes;
   const runeList = runes.map((rune) =>
@@ -11,10 +12,25 @@ const RuneList = (props) => {
   );
 }
 
+// get all equipped runes
+const EquippedRuneList = (props) => {
+  const units = testData.unit_list;
+  const equippedRuneList = units.map((unit) =>
+    unit.runes.map(rune =>
+      <li>{rune.rune_id}</li>
+    )
+  )
+  return (
+    <ul>{equippedRuneList}</ul>
+  );
+}
+
 const App = () => {
+  console.log(testData);
   return (
     <div className="App">
       <RuneList />
+      <EquippedRuneList />
     </div>
   );
 }

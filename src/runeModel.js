617c6d395f3e89abfc8ca7d24fd4a380f69c52d5
Allen <headhuntar@gmail.com>
2020-10-19 18:56:48 -0700

    how to read json for runes

---
+++ src/runeModel.js
@@ -0,0 +1,49 @@
+const rune = {
+  "occupied_type": 1,
+  "occupied_id": 10695054201, // 0 or monster id
+  "sell_value": 31383,
+  "pri_eff": [
+    10,
+    80
+  ],
+  "prefix_eff": [
+    0,
+    0
+  ],
+  "slot_no": 4,
+  "rank": 0, // unknown
+  "sec_eff": [
+    [
+      8, // set
+      8, // value
+      0,
+      0
+    ],
+    [
+      4,
+      7,
+      0,
+      0
+    ],
+    [
+      11,
+      7,
+      0,
+      0
+    ],
+    [
+      6,
+      12,
+      0,
+      0
+    ]
+  ],
+  "upgrade_curr": 15,
+  "class": 6, // stars
+  "set_id": 5, // set
+  "upgrade_limit": 15,
+  "rune_id": 9847142821,
+  "extra": 0 // grade
+}
+
+export default rune;

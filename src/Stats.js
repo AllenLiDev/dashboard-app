cae26a1c8c7ec48ac49f77ee9c4d978a4e3026c2
Allen <headhuntar@gmail.com>
2020-10-29 21:14:42 -0700

    create stat component

---
+++ src/Stats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = (props) => {
+  const fastestRunes = props.fastest;
+  console.log(fastestRunes);
+  return (
+    <div>works</div>
+  )
+}
+
+export default Stats;

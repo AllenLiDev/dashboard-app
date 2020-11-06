5fd997f1004c96b5e1383f2830f5a513fdfdb8e7
Allen <headhuntar@gmail.com>
2020-11-05 17:22:57 -0800

    update with more test and learn more about jest

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,5 +1,12 @@
 const { getSetName, getStatName, getSetId, getStatId, getRuneGrade, fastestInSet } = require('./helper');
 
-test('returns Energy for setID 1', () => {
+test('returns Energy for setId = 1', () => {
   expect(getSetName(1)).toBe('Energy');
 });
+
+test('return SPD for statId = 8', () => {
+  expect(getStatName(8)).toBe('SPD');
+});
+
+// describe think component, group with this
+// it think indivudual part
